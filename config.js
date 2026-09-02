2c084869aa99de03d92987d7e789c489501b9e85
xavierguillon2007-dev <xavierguillon2007@gmail.com>
2026-09-02 19:27:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration Supabase
 // Remplace ces deux valeurs par celles de ton projet Supabase.
 // Elles se trouvent dans Supabase > Project Settings > API.
-window.SUPABASE_URL = "https://TON-PROJET.supabase.co";
+window.SUPABASE_URL = "https://roibvzrnfwvuiuaxeuqp.supabase.co/rest/v1/";
 window.SUPABASE_ANON_KEY = "sb_publishable_h6mKaDMdd3lW4_J9xKxVOg_aiUW1kwb";
