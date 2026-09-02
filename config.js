305751eb17995b5bfeb8479624a1106dd7f3d479
xavierguillon2007-dev <xavierguillon2007@gmail.com>
2026-09-02 21:29:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Récupère ces valeurs dans Supabase > Settings > API Keys.
 // IMPORTANT : utilise la Publishable key (sb_publishable_...).
 window.SUPABASE_URL = "https://roibvzrnfwvuiuaxeuqp.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY = "TA_CLE_PUBLISHABLE";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_h6mKaDMdd3lW4_J9xKxVOg_aiUW1kwb";
